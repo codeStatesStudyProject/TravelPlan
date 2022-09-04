96914c7ea09d812838cfe66ff584711a656976e5
kexxxon <dev.kexon@gmail.com>
2022-09-04 22:43:18 +0900

    #3. feat: add auditing

--- src/main/java/travelplanrepo/TravelPlanRepoApplication.java
+++ src/main/java/travelplanrepo/TravelPlanRepoApplication.java
@@ -2,11 +2,14 @@ package travelplanrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TravelPlanRepoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelPlanRepoApplication.class, args);
     }
+
 }
