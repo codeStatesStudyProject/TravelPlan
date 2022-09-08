4ae67e0f901cd5b2d24de0475d0a87d6497ec5cf
hipopotamus <qkqntjddn@gmail.com>
2022-09-09 02:26:28 +0900

    [Feat] FileProcessor 구현

---
+++ src/main/java/travelplanrepo/common/utill/File/FileProcessor.java
@@ -0,0 +1,39 @@
+package travelplanrepo.common.utill.File;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class FileProcessor {
+
+    public File storeFile(MultipartFile multipartFile, String path) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalFilename = multipartFile.getOriginalFilename();
+        String storeFileName = getStoreFileName(originalFilename);
+        String fullPath = getFullPath(path, storeFileName);
+
+        multipartFile.transferTo(new java.io.File(fullPath));
+        return new File(originalFilename, storeFileName, fullPath);
+    }
+
+    private String getStoreFileName(String originalFilename) {
+        UUID uuid = UUID.randomUUID();
+        String ext = getExt(originalFilename);
+        return uuid + "." + ext;
+    }
+
+    private static String getExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+
+    private String getFullPath(String path, String storeFileName) {
+        return path + storeFileName;
+    }
+}
