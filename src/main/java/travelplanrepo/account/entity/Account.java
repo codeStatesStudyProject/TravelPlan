5db40149cce4042aaf3a0cb8e268c65432dc1583
Grooomit <tmd5972@gmail.com>
2022-09-18 16:40:31 +0900

    [Feat] Account Entity 구현

--- src/main/java/travelplanrepo/account/entity/Account.java
+++ src/main/java/travelplanrepo/account/entity/Account.java
@@ -3,9 +3,14 @@ package travelplanrepo.account.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import travelplanrepo.board.entity.Board;
 import travelplanrepo.common.auditing.BaseTime;
+import travelplanrepo.utill.File.File;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,9 +20,16 @@ public class Account extends BaseTime {
     @Id @GeneratedValue
     @Column(name = "account_id")
     private Long id;
-
     private String email;
     private String password;
+    private String nickName;
+    private File profileImg;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer age;
+    private String mobile;
 
     @Enumerated(EnumType.STRING)
     private Role role;
