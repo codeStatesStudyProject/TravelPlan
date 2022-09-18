021a294231003b49895d69c177e2871b8f105acb
hipopotamus <qkqntjddn@gmail.com>
2022-09-18 16:08:07 +0900

    [Feat] Board 등록 API 구현

--- src/main/java/travelplanrepo/board/entity/Board.java
+++ src/main/java/travelplanrepo/board/entity/Board.java
@@ -1,16 +1,19 @@
 package travelplanrepo.board.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 import travelplanrepo.account.entity.Account;
 import travelplanrepo.common.auditing.BaseTime;
 import travelplanrepo.itinerary.entity.Itinerary;
 import travelplanrepo.utill.File.File;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 public class Board extends BaseTime {
     @Id @GeneratedValue
     @Column(name = "board_id")
@@ -20,9 +23,10 @@ public class Board extends BaseTime {
     private Account account;
 
     @OneToMany(mappedBy = "board", cascade = CascadeType.PERSIST)
-    private List<Itinerary> itineraryList;
+    private List<Itinerary> itineraryList = new ArrayList<>();
 
     private String title;
     private String preface;
     private File thumbnail;
+
 }
