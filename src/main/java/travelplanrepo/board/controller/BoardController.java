18bbba884600835a90f234a73bbaecbbf9a0c6ed
박성우 <77265289+hipopotamus@users.noreply.github.com>
2022-09-18 16:15:06 +0900

    [Feat] Board 등록 API 구현

---
+++ src/main/java/travelplanrepo/board/controller/BoardController.java
@@ -0,0 +1,52 @@
+package travelplanrepo.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import travelplanrepo.board.dto.PostBoardDto;
+import travelplanrepo.board.entity.Board;
+import travelplanrepo.board.service.BoardService;
+import travelplanrepo.itinerary.dto.PostItineraryDto;
+import travelplanrepo.itinerary.entity.Itinerary;
+import travelplanrepo.security.argumentresolver.LoginAccountId;
+import travelplanrepo.utill.File.File;
+import travelplanrepo.utill.File.FileProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    @Value("${file.boardImg}")
+    private String boardImgPath;
+
+    @Value("${file.scheduleImg}")
+    private String scheduleImgPath;
+
+    private final BoardService boardService;
+    private final FileProcessor fileProcessor;
+
+    @PostMapping("/board")
+    public String postBoard(@LoginAccountId Long accountId, @ModelAttribute PostBoardDto postBoardDto) throws IOException {
+        Board board = postBoardDto.toBoard();
+        File thumbnail = fileProcessor.storeFile(postBoardDto.getThumbnail(), boardImgPath);
+        board.setThumbnail(thumbnail);
+
+        List<PostItineraryDto> itineraryList = postBoardDto.getItineraryList();
+        List<Itinerary> boardItineraryList = board.getItineraryList();
+        for (int i = 0; i < itineraryList.size(); i++) {
+            PostItineraryDto postItineraryDto = itineraryList.get(i);
+            File img = fileProcessor.storeFile(postItineraryDto.getImg(), scheduleImgPath);
+
+            Itinerary itinerary = boardItineraryList.get(i);
+            itinerary.setImg(img);
+        }
+
+        boardService.signBoard(accountId, board);
+        return "success board created";
+    }
+}
