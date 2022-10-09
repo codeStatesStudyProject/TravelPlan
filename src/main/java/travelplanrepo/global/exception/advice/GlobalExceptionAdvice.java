4e308618136d6a47c6655c04f532c50ffa2384ad
kexxxon <dev.kexon@gmail.com>
2022-10-10 03:23:33 +0900

    [Feat] GlobalExceptionAdvice 추가 - BindException 발생 시 출력되는 에러 메시지를 Exception 형식에 맞게 변경

--- src/main/java/travelplanrepo/global/exception/advice/GlobalExceptionAdvice.java
+++ src/main/java/travelplanrepo/global/exception/advice/GlobalExceptionAdvice.java
@@ -3,6 +3,7 @@ package travelplanrepo.global.exception.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import travelplanrepo.global.exception.dto.ErrorResponse;
@@ -13,6 +14,13 @@ public class GlobalExceptionAdvice {
     @ExceptionHandler
     public ResponseEntity allExceptionHandler(Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(400, e.getClass().getSimpleName(), e.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity bindExceptionHandler(BindException be) {
+        ErrorResponse errorResponse = new ErrorResponse(400, be.getClass().getSimpleName(),"잘못된 입력값입니다.");
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
